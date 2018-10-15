30fc1ab517af779cf1fe2ab46a673863fe55cdb8
Magnus <magnusrrc@gmail.com>
2018-10-15 16:19:18 +0200

    Adding ball images

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,13 @@
 package com.londonappbrewery.magiceightball;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Import assets
+        final ImageView ball = (ImageView) findViewById(R.id.ball);
+        Button button = (Button) findViewById(R.id.button);
+
+        // Create image array
+        final int[] ballImgArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        // On click
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rand = new Random();
+                int number = rand.nextInt(5);
+                ball.setImageResource(ballImgArray[number]);
+            }
+        });
     }
 }
